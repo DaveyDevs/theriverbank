4d0f790d8fb4ed7abe128095a238641b1cbae284
DaveyDevs <daveydevs@gmail.com>
2020-04-04 21:14:38 -0500

    Adding site links to single event listing

--- app/public/wp-content/themes/bento-child/functions.php
+++ app/public/wp-content/themes/bento-child/functions.php
@@ -49,3 +49,147 @@ add_filter('get_event_listings_query_args', 'theme_name_custom_orderby_query_arg
 add_filter('event_manager_delete_expired_events_days', function () {
     return 0;
 });
+
+
+// Getting links from custom fields
+
+// Website
+
+function get_website_link($post = null)
+{
+    $post = get_post($post);
+
+    if ($post->post_type !== 'event_listing')
+        return;
+    return apply_filters('display_website_link', $post->_website_link, $post);
+}
+
+function display_website_link($before = '', $after = '', $echo = true, $post = null)
+{
+    $event_website_link = get_website_link($post);
+
+    if (strlen($event_website_link) == 0)
+        return;
+
+    $event_website_link = esc_attr(strip_tags($event_website_link));
+
+    $event_website_link = $before . $event_website_link . $after;
+
+    if ($echo)
+        echo $event_website_link;
+    else
+        return $event_website_link;
+}
+
+// YouTube
+
+function get_youtube_link($post = null)
+{
+    $post = get_post($post);
+
+    if ($post->post_type !== 'event_listing')
+        return;
+    return apply_filters('display_youtube_link', $post->_youtube_link, $post);
+}
+
+function display_youtube_link($before = '', $after = '', $echo = true, $post = null)
+{
+    $event_youtube_link = get_youtube_link($post);
+
+    if (strlen($event_youtube_link) == 0)
+        return;
+
+    $event_youtube_link = esc_attr(strip_tags($event_youtube_link));
+
+    $event_youtube_link = $before . $event_youtube_link . $after;
+
+    if ($echo)
+        echo $event_youtube_link;
+    else
+        return $event_youtube_link;
+}
+
+
+//Facebook
+
+function get_facebook_link($post = null)
+{
+    $post = get_post($post);
+
+    if ($post->post_type !== 'event_listing')
+        return;
+    return apply_filters('display_facebook_link', $post->_facebook_link, $post);
+}
+
+function display_facebook_link($before = '', $after = '', $echo = true, $post = null)
+{
+    $event_facebook_link = get_facebook_link($post);
+
+    if (strlen($event_facebook_link) == 0)
+        return;
+
+    $event_facebook_link = esc_attr(strip_tags($event_facebook_link));
+
+    $event_facebook_link = $before . $event_facebook_link . $after;
+
+    if ($echo)
+        echo $event_facebook_link;
+    else
+        return $event_facebook_link;
+}
+
+//Instagram
+
+function get_instagram_link($post = null)
+{
+    $post = get_post($post);
+
+    if ($post->post_type !== 'event_listing')
+        return;
+    return apply_filters('display_instagram_link', $post->_instagram_link, $post);
+}
+
+function display_instagram_link($before = '', $after = '', $echo = true, $post = null)
+{
+    $event_instagram_link = get_instagram_link($post);
+
+    if (strlen($event_instagram_link) == 0)
+        return;
+
+    $event_instagram_link = esc_attr(strip_tags($event_instagram_link));
+
+    $event_instagram_link = $before . $event_instagram_link . $after;
+
+    if ($echo)
+        echo $event_instagram_link;
+    else
+        return $event_instagram_link;
+}
+
+//Twitter
+
+function get_twitter_link($post = null)
+{
+    $post = get_post($post);
+
+    if ($post->post_type !== 'event_listing')
+        return;
+    return apply_filters('display_twitter_link', $post->_twitter_link, $post);
+}
+
+function display_twitter_link($before = '', $after = '', $echo = true, $post = null)
+{
+    $event_twitter_link = get_twitter_link($post);
+
+    if (strlen($event_twitter_link) == 0)
+        return;
+
+    $event_twitter_link = esc_attr(strip_tags($event_twitter_link));
+
+    $event_twitter_link = $before . $event_twitter_link . $after;
+
+    if ($echo)
+        echo $event_twitter_link;
+    else
+        return $event_twitter_link;
+}
